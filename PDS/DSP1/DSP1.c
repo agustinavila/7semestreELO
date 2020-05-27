e6c7696d516ff0574d4b1b4dbf5824c3570cbf81
Agustin Avila <tinto.avila@gmail.com>
2020-05-27 11:25:45 -0300

    Creada funcion que obtiene el valor medio

--- PDS/DSP1/DSP1.c
+++ PDS/DSP1/DSP1.c
@@ -2,31 +2,40 @@
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
-//char *getcwd(char *buf, size_t size);
+
 FILE *abrir_archivo(char nombre[], char modo[])
 {
-    FILE *arch;
+    FILE *archivo;
     char string[100];
     char cwd[PATH_MAX];
-   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       printf("Current working dir: %s\n", cwd);
-   } else {
-       perror("getcwd() error");
-   }
+    getcwd(cwd, sizeof(cwd));
     strcpy(string, cwd);
     strcat(string,"\\");
     strcat(string, nombre);
-    arch = fopen(string, modo);
+    archivo = fopen(string, modo);
     printf("%s\n",string);
-    if (arch == NULL)
+    if (archivo == NULL)
     {
-        puts("NO SE PUEDE ABRIR EL ARCHIVO ESPECIFICADO\n");
+        printf("NO SE PUEDE ABRIR EL ARCHIVO ESPECIFICADO\n");
     }
-    return arch;
-    fclose(arch);
+    return archivo;
+    fclose(archivo);
 }
 
-
+float valor_medio(FILE *archivo)
+{
+    int n = 0;
+    float suma = 0, actual;
+    fscanf(archivo,"%f\n", &actual);        //salta la frecuencia
+    while (!feof(archivo))
+    {
+        fscanf(archivo, "%f\n", &actual);
+        suma += actual;
+        n++;
+    }
+    rewind(archivo);              
+    return (suma/n);
+}
 
 
 
@@ -35,9 +44,23 @@ FILE *abrir_archivo(char nombre[], char modo[])
 int main() {
     FILE *arch1, *arch2, *arch3, *arch4, *arch5;
     float minimo, maximo, freq;
+    //abre los archivos
     arch1 = abrir_archivo("Signal_01.txt", "r");
     arch2 = abrir_archivo("Signal_02.txt", "r");
     arch3 = abrir_archivo("Signal_03.txt", "r");
     arch4 = abrir_archivo("Signal_04.txt", "r");
     arch5 = abrir_archivo("Signal_05.txt", "r");
+    //prueba del valor medio
+    float medio=valor_medio(arch1);
+    
+    printf("vmedio= %f",medio);
+    //tengo que calcular valor medio de cada archivo y restarlo
+    //de cada archivo. guardarlo en un archivo nuevo.
+    //el primer valor tiene la frecuencia de muestreo.
+    //de ahi para abajo son las medidas en volts
+
+    //sumar y restar dos señales y guardar resultados
+    //calcular frecuencias y valores maximos y minimos
+    //realizar autocorrelacion y correlacion entre 2
+    //graficar en matlab xd
 }
