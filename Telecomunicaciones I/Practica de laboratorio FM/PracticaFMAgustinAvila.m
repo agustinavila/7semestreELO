bab32fcd7065962333042de6deb136b25c6a51cc
Agustin Avila <tinto.avila@gmail.com>
2020-06-02 15:25:38 -0300

    Entregada practica de FM

--- Telecomunicaciones I/Practica de laboratorio FM/PracticaFMAgustinAvila.m
+++ Telecomunicaciones I/Practica de laboratorio FM/PracticaFMAgustinAvila.m
@@ -1,18 +1,23 @@
 load_system('ModulacionFM.slx');
-wc=2800;
 f_delta=50;
-w_asignada=725;
-set_param('ModulacionFM/sw1', 'sw', '1');%setea el switch para moduladora seno
-sim('ModulacionFM.slx');
-pause;
-set_param('ModulacionFM/sw1', 'sw', '0');%setea el switch para sawtooth
-sim('ModulacionFM.slx');
-pause;
+wc_asignada=725;
+wc=wc_asignada;
+% set_param('ModulacionFM/sw1', 'sw', '1');%setea el switch para moduladora seno
+% sim('ModulacionFM.slx');
+% pause;
+% set_param('ModulacionFM/sw1', 'sw', '0');%setea el switch para sawtooth
+% sim('ModulacionFM.slx');
+% pause;
+
+
+% %% punto 2
+% f_delta=400;
 
 %%punto 3:
 wc=2000+wc_asignada;
+f_delta=50;
 set_param('ModulacionFM/Moduladora: Tono', 'frequency', '300')
 set_param('ModulacionFM/sw1', 'sw', '1');%setea el switch para moduladora seno
 sim('ModulacionFM.slx');
-set_param('ModulacionFM/sw1', 'sw', '0');%setea el switch para sawtooth
-sim('ModulacionFM.slx');
+% set_param('ModulacionFM/sw1', 'sw', '0');%setea el switch para sawtooth
+% sim('ModulacionFM.slx');
