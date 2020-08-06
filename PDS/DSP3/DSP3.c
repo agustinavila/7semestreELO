4fb48ad05793d08296ff8d1d04fca45e7be2c334
Agustin Avila <tinto.avila@gmail.com>
2020-08-06 15:16:48 -0300

    El programa en C recibe argumentos, es mas general

--- PDS/DSP3/DSP3.c
+++ PDS/DSP3/DSP3.c
@@ -7,33 +7,36 @@
 //#include <math.h>
 #define pi 3.14159265
 
-FILE *abrir_archivo(char modo[], int f, char extra[]) //abre los archivos en el modo indicado, se le puede agregar un sufijo al nombre
+FILE *abrir_archivo(char modo[], char nombre[], char extra[]) //abre los archivos en el modo indicado, se le puede agregar un sufijo al nombre
 {
     FILE *archivo;
-    char string[100];
+    char string[150];
     char cwd[PATH_MAX];
-    getcwd(cwd, sizeof(cwd));                                               //obtiene el directorio donde se esta ejecutando
-    snprintf(string, sizeof(string), "%s\\Tono_%dHz%s.txt", cwd, f, extra); //siguen el patron original, solo les cambia el numero
+    getcwd(cwd, sizeof(cwd));                                             //obtiene el directorio donde se esta ejecutando
+    snprintf(string, sizeof(string), "%s\\%s%s.txt", cwd, nombre, extra); //siguen el patron original, solo les cambia el numero
     archivo = fopen(string, modo);
     if (archivo == NULL)
     {
-        printf("NO SE PUEDE ABRIR EL ARCHIVO ESPECIFICADO\n"); //no tiene mucho sentido porque no hace nada
+        printf("No se pudo abrir el archivo \"%s%s.txt\"\n", nombre, extra); //no tiene mucho sentido porque no hace nada
     }
     return archivo;
 }
-
-void transformada(FILE *archivo_original,int offset,int N, int f) //resta el promedio a todo el archivo
+void transformada(FILE *archivo_original, int offset, int N, char nombre[])
+// Los argumentos son: *archivo_original es el archivo a analizar
+// offset es un offset a partir del cual se toman los datos
+// N es la cantidad de puntos con los cuales realiza la transformada
+// nombre es el nombre del archivo original
 {
     float x[N], freq, salida = 0;
     int k = 0, n = 0;
     float Im = 0, Re = 0;
     FILE *archivo_nuevo;
-    char extra[25];
-    snprintf(extra, sizeof(extra), "_Transformada%d", N);
-    archivo_nuevo = abrir_archivo("w+", f, extra); //abre un nuevo archivo
-    fscanf(archivo_original, "%f\n", &freq);       //obtiene la frecuencia
-    fprintf(archivo_nuevo, "%.1f\n", freq);        //y la guarda en el nuevo
-       for (n = 0; n < offset; n++)
+    char extra[50];
+    snprintf(extra, sizeof(extra), "_Transformada%dPuntos", N);
+    archivo_nuevo = abrir_archivo("w+", nombre, extra); //abre un nuevo archivo
+    fscanf(archivo_original, "%f\n", &freq);            //obtiene la frecuencia
+    fprintf(archivo_nuevo, "%.1f\n", freq);             //y la guarda en el nuevo
+    for (n = 0; n < offset; n++)
     {
         x[0] = 0;
         fscanf(archivo_original, "%f\n", &x[0]); //lee el arreglo con los valores
@@ -41,39 +44,53 @@ void transformada(FILE *archivo_original,int offset,int N, int f) //resta el pro
     for (n = 0; n < N; n++)
     {
         x[n] = 0;
-        fscanf(archivo_original, "%f\n", &x[n]); //lee el arreglo con los valores
+        fscanf(archivo_original, "%f\n", &x[n]); //guarda los valores en el arreglo
     }
+
+    //a partir de este punto es la transformada propiamente dicha
     for (k = 0; k < N; k++)
     {
         for (n = 0; n < N; n++)
         {
-            Im = (Im + x[n] * (sin((2 * pi * n * k) / N)));
+            Im = (Im + x[n] * (sin((2 * pi * n * k) / N))); //realiza el calculo
             Re = (Re + x[n] * (cos((2 * pi * n * k) / N)));
         }
         salida = sqrt(pow(Re, 2) + pow(Im, 2));
-        fprintf(archivo_nuevo, "%.2f\n", salida);
+        fprintf(archivo_nuevo, "%.2f\n", salida); //guarda el resultado en el archivo
         Im = 0;
         Re = 0;
     }
     fclose(archivo_nuevo); //cierra el archivo nuevo
     rewind(archivo_original);
 }
-int main()
+int main(int argc, char *argv[])
 {
-    int N, i;
-    int f[] = {20, 50, 200};
+
+    int N,offset;
     FILE *arch;
-    for (i = 0; i < 3; i++)
+    if (argc == 4)
     {
-        arch = abrir_archivo("r", f[i], ""); //abre archivo
-        transformada(arch,10, 8, f[i]);
-        transformada(arch,10, 16, f[i]);
-        transformada(arch,10, 256, f[i]);
-        fclose(arch);
+        N=atoi(argv[2]);
+        offset=atoi(argv[3]);
+        printf("Se calculara la TDF del archivo %s con %d muestras y un offset de %d\n", argv[1], N, offset);
     }
-        arch = abrir_archivo("r",11025, "Perro"); //abre archivo
-        transformada(arch,1000, 8, 11025);
-        transformada(arch,1000, 16, 11025);
-        transformada(arch,1000, 4096, 11025);
-        fclose(arch);
+    else if (argc > 4)
+    {
+        printf("Demasiados argumentos.\n");
+        return(1);
+    }
+    else
+    {
+        printf("No se pasaron argumentos\n");
+        printf("El programa se debe correr de la siguiente manera:\n");
+        printf("DSP3.exe archivo_a_analizar cant_muestras offset\n");
+        printf("Por ejemplo para el archivo \"Tono_50Hz.txt\" con 16 muestras y un offset de 200:\n");
+        printf("\"DSP3.exe Tono_50Hz 16 200\"\n");
+        printf("(No se debe incluir la terminacion .txt del archivo)\n");
+        return(1);
+    }
+    arch = abrir_archivo("r", argv[1], ""); //abre archivo
+    transformada(arch, offset, N, argv[1]);
+    fclose(arch);
+    printf("Transformada calculada con exito.\n");
 }
